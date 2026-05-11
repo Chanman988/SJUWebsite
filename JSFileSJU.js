66532767cb7e6256fcd02f531b1d49596e62ac51
Ashen One <104659050+Chanman988@users.noreply.github.com>
2026-05-11 19:40:18 -0400

    Update JSFileSJU.js

--- JSFileSJU.js
+++ JSFileSJU.js
@@ -1,1 +1,5 @@
-
+const button = document.getElementById("comicSans");
+const text = document.getElementById("pr_players");
+   button.addEventListener("click", function() {
+    text.style.fontFamily = "Comic Sans MS";
+});
